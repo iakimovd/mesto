d925f323fb13f8b834cb91a862edbb4d4b790561
Dmitrii <dimitry.vs@gmail.com>
2022-04-03 18:46:54 +0500

    fix: change BEM structure

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 let content = document.querySelector('.content');
-let editButton = content.querySelector('.profile__info_edit-button');
-let saveButton = document.querySelector('.popup__container_save-button');
+let editButton = content.querySelector('.profile__edit-button');
+let saveButton = document.querySelector('.popup__save-button');
 let closeButton = document.querySelector('.popup__close-button');
 let profileInfo = content.querySelector('.profile__info');
 let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__container_name-edit');
-let jobInput = formElement.querySelector('.popup__container_profession-edit');
-let nameEdit = document.querySelector('.profile__info_name');  // Выберите элементы, куда должны быть вставлены значения полей
-let jobEdit = document.querySelector('.profile__info_profession');
+let nameInput = formElement.querySelector('.popup__name-edit');
+let jobInput = formElement.querySelector('.popup__profession-edit');
+let nameEdit = document.querySelector('.profile__name');
+let jobEdit = document.querySelector('.profile__profession');
 
 editButton.addEventListener('click', popupActivate);
 closeButton.addEventListener('click', popupClose);
@@ -25,13 +25,11 @@ function popupClose() {
 
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  let nameEdit = document.querySelector('.profile__info_name');  // Выберите элементы, куда должны быть вставлены значения полей
-  let jobEdit = document.querySelector('.profile__info_profession');
+  let nameEdit = document.querySelector('.profile__name');
+  let jobEdit = document.querySelector('.profile__profession');
   nameEdit.textContent = nameInput.value;
   jobEdit.textContent = jobInput.value;
   popupClose();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 
